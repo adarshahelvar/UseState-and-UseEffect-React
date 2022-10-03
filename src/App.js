c1209dd8668d6aba378a4d0de62eea9a78799857
Adarsha A <adarshaadi1997@gmail.com>
2022-10-03 18:50:05 +0530

    Hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from 'react';
+import {data} from './data';
+
+const url = 'https://api.github.com/users';
 
 function App() {
+  const [gusers, setGusers] = useState([]);
+  async function getData() {
+    const response =  await fetch(url);
+    const users = await response.json();
+    setGusers(users);
+  }
+
+  useEffect(()=>{
+    getData();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {gusers.map((gusers)=>{
+      return (
+        <li>{gusers.login}</li>
+      )
+    })}
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        id: 1,
+        name: 'adi'
+    },
+    {
+        id:2,
+        name:'rakku'
+    },
+    {
+        id:3,
+        name: 'Subhash'
+    }
+];
